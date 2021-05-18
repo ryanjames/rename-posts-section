b1f290fbc76e4ba691ac9367231a7be0c0942386
ryanjames <ryan@ryanjam.es>
2021-05-18 16:29:02 -0700

    Rename News to Blog

--- plugin.php
+++ plugin.php
@@ -25,17 +25,17 @@ add_action( 'init', 'cp_change_post_object' );
 function cp_change_post_object() {
     $get_post_type = get_post_type_object('post');
     $labels = $get_post_type->labels;
-        $labels->name = 'News';
-        $labels->singular_name = 'News';
-        $labels->add_new = 'Add News';
-        $labels->add_new_item = 'Add News';
-        $labels->edit_item = 'Edit News';
+        $labels->name = 'Blog';
+        $labels->singular_name = 'Blog';
+        $labels->add_new = 'Add Post';
+        $labels->add_new_item = 'Add Post';
+        $labels->edit_item = 'Edit Post';
         $labels->new_item = 'News';
-        $labels->view_item = 'View News';
-        $labels->search_items = 'Search News';
+        $labels->view_item = 'View Post';
+        $labels->search_items = 'Search Blog';
         $labels->not_found = 'No News found';
         $labels->not_found_in_trash = 'No News found in Trash';
-        $labels->all_items = 'All News';
-        $labels->menu_name = 'News';
-        $labels->name_admin_bar = 'News';
+        $labels->all_items = 'All Posts';
+        $labels->menu_name = 'Blog';
+        $labels->name_admin_bar = 'Blog';
 }
